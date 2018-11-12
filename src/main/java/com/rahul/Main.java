7c0fa0440bb4803247570b932de74fecdbf8c426
Rahul Srivastava <rahsriv@amazon.com>
2018-11-12 10:09:12 -0500

    Hello world

--- src/main/java/com/rahul/Main.java
+++ src/main/java/com/rahul/Main.java
@@ -1,8 +1,14 @@
 package com.rahul;
 
+import io.reactivex.Observable;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    hello("Iron Man", "The Hulk", "Thor");
+    }
+
+    public static void hello(String... names) {
+        Observable.fromArray(names).subscribe(s -> System.out.println("Hello " + s));
     }
 }
